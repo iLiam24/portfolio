a1ec92160a7a1c2dee5262be7fb9a89e16317f91
iLiam24 <163138731+iLiam24@users.noreply.github.com>
2024-04-02 10:42:40 +0200

    clean + setup

---
+++ portfolio/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/Hero.vue'
+</script>
+
+<template>
+  <header>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
